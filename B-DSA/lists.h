9b995b8b6929a48092715385065299b4690a6fff
BrianEtemesi <etemecci@gmail.com>
2022-10-25 16:40:09 +0000

    define structure "mycars"

--- B-DSA/lists.h
+++ B-DSA/lists.h
@@ -1,5 +1,17 @@
 #ifndef LIST_H
 #define LIST_H
 
+/**
+ * struct my_cars
+ * @make: car make
+ * @year: year of manufucture
+ */
+
+typedef struct mycars
+{
+	char *make;
+	int year;
+	struct mycars *next;
+} my_cars;
 
 #endif /* LIST_H */
