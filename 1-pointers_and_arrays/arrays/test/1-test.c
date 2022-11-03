23eaee140af55a81ebe2a665f4058b7d066f094d
BrianEtemesi <etemecci@gmail.com>
2022-11-03 12:26:20 +0000

    add function msize to get size/len of binary

--- 1-pointers_and_arrays/arrays/test/1-test.c
+++ 1-pointers_and_arrays/arrays/test/1-test.c
@@ -5,19 +5,50 @@
  * main - entry point
  * Return: 0
  */
+int memsize(int n);
 
 int main(void)
 {
 	int *arr;
+	int rem, i = 0, x = 6;
+	unsigned long int n = 8;
+	int size = memsize(n);
 
-	arr = malloc((sizeof(int)));
+	printf("size of binary is %d bits\n", size);
+
+	arr = malloc((sizeof(int) * size));
 	if (arr == NULL)
 		return (-1);
 
-	arr[0] = 1;
-	arr[1] = 2;
+	while (n != 0)
+	{
+		rem = n % 2;
+		if (rem != 1)
+			rem = x;
+		arr[i] = rem;
+		n = n / 2;
+		i++;
+	}
 
-	printf("arr[1] = %d\n", arr[1]);
+	for (i = 0; arr[i]; i++)
+	{
+		printf("%d", arr[i]);
+	}
+
+	printf("\n");
 
 	return (0);
+}
+
+int memsize(int n)
+{
+	int i = 0;
+
+	while (n != 0)
+	{
+		n = n / 2;
+		i++;
+	}
+
+	return (i);
 }
