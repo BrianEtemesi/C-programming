82c710dd281fc42f64cd78579e9544084ae2d88b
BrianEtemesi <etemecci@gmail.com>
2022-11-15 09:54:54 +0000

    write program to test string tokenization

---
+++ E-simple_shell/5-strtok.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - enty point / string tokenization
+ * Return: 0
+ */
+
+int main(void)
+{
+	char string[] = "This new world, New world of tech";
+	const char del[] = ",";
+	char *mytoken;
+
+	mytoken = strtok(string, del);
+	while (mytoken != NULL)
+	{
+		printf("%s\n", mytoken);
+		mytoken = strtok(NULL, del);
+	}
+
+	return (0);
+}
