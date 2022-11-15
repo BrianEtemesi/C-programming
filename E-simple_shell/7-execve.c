a68972ab4a038ffd806af15e52c077906edacda4
BrianEtemesi <etemecci@gmail.com>
2022-11-15 15:07:37 +0000

    test execvp

---
+++ E-simple_shell/7-execve.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - use execve to execute 2-max_pid(from current directory)
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *args[] = {"./2-max_pid", NULL};
+
+	execvp(args[0], args);
+
+	/* all statements are ignored after calling execve */
+	/* the whole process is replaced by 2-max_pid */
+
+	printf("This will not print after the execve call");
+
+	return (0);
+}
