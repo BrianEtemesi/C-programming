80c918aedff732bb42d06b27b302a9ff9c9563b9
BrianEtemesi <etemecci@gmail.com>
2022-11-16 17:50:19 +0000

    wait system call

---
+++ E-simple_shell/B-wait.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * main - test wait sys call
+ * Return: 0
+ */
+
+int main(void)
+{
+	pid_t p;
+
+	printf("before fork\n");
+	p = fork();
+
+	if (p == 0)/* child */
+	{
+		printf("I am child with id %d\n", getpid());
+		printf("My parent's id is %d\n", getppid());
+	}
+	else/* parent */
+	{
+		wait(NULL);
+		printf("My child's id is %d\n", p);
+		printf("I am a parent having id %d\n", getpid());
+	}
+
+	printf("Common\n");
+
+	return (0);
+}
