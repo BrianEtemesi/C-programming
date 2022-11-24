caee0bf6309f05fc5bd7150ff4b515289fc4c934
BrianEtemesi <etemecci@gmail.com>
2022-11-24 07:13:52 +0000

    add splitline to tokenize line to separate arguments, test successful

--- E-simple_shell/main.h
+++ E-simple_shell/main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include<stdlib.h>
-
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " \t\r\n\a"
+#include <stdlib.h>
+#include <string.h>
 
 void _loop(void);
 char *_readline(void);
+char **_splitline(char *line);
 
 /**
  * _loop - read, parse and executed parsed commands
@@ -13,13 +15,24 @@ char *_readline(void);
 
 void _loop(void)
 {
+	int i;
 	char *line;
+	char **args;
 
 	do
 	{
 		printf("$ ");
 		line = _readline();
-		printf("%s", line);
+		printf("You entered: %s\nTokenized to:\n", line);
+		args = _splitline(line);
+
+		for (i = 0; args[i]; i++)
+		{
+			printf("%s\n", args[i]);
+		}
+
+		free(line);
+		free(args);
 	} while (1);
 }
 
@@ -47,5 +60,63 @@ char *_readline(void)
 	}
 	return line;
 }
+
+/**
+ * _splitline - splits line to arguments
+ * @line: line to be tokenized
+ * Return: Pointer to an array of pointers/ list of arguments
+ */
+
+char **_splitline(char *line)
+{
+	int bufsize = TOK_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *token;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "lsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, TOK_DELIM);
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+
+		if (position >= bufsize)
+		{
+			bufsize += TOK_BUFSIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char*));
+			if (!tokens)
+			{
+				fprintf(stderr, "lsh: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, TOK_DELIM);
+	}
+	tokens[position] = NULL;
+	return (tokens);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif /* MAIN_H */
